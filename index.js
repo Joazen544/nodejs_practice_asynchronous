c98017ee7cd47673b3f3470f6a38ad1e03f4d2f5
Joazen <johnsons544@gmail.com>
2023-08-06 23:18:56 +0800

    start to learn promise. Have error calling api

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const superagent = require("superagent");
 
 let obj = new Map();
 let samePic = 0;
-for (let i = 0; i < 10; i++) {
+let counter = 0;
+while (counter < 10) {
   superagent.get(`https://dog.ceo/api/breeds/image/random`).end((err, res) => {
-    //console.log(res);
-    if (!obj.get(res.body.message)) {
-      obj.set(res.body.message);
-      fs.appendFile("dog-image.txt", `${res.body.message}\n`, (err) => {
-        if (err) return console.log(err.message);
-        console.log(`${i} random dog image saved to file`);
-      });
-    } else {
-      samePic++;
-      console.log(samePic);
+    try {
+      if (!obj.get(res.body.message)) {
+        obj.set(res.body.message);
+        fs.appendFile("dog-image.txt", `${res.body.message}\n`, (err) => {
+          if (err) return console.log(err.message);
+          console.log(`${counter} random dog image saved to file`);
+          counter++;
+        });
+      } else {
+        samePic++;
+        console.log(samePic);
+      }
+    } catch (e) {
+      console.log("There is an error!!!!!");
+      console.log(res);
     }
   });
 }
