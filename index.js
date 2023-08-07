442eb8ace5326fe5f33fa3b0e2e813aeb0d60f05
Joazen <johnsons544@gmail.com>
2023-08-07 11:19:53 +0800

    use promise to chain the asynchronous process

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-let obj = new Map();
-let samePic = 0;
 let counter = 0;
 let countNum = 0;
 let ifPrintErr = false;
@@ -17,32 +15,49 @@ const readFilePro = (file) => {
   });
 };
 
-//for (let i = 0; i < 10; i++) {
-while (counter < 500) {
-  counter++;
-  superagent
-    .get(`https://dog.ceo/api/breeds/image/random`)
-    .then((res) => {
-      // "then" only works when res exists
-      //if (!obj.get(res.body.message)) {
-      //  obj.set(res.body.message);
-      fs.appendFile("dog-image.txt", `${res.body.message}\n`, (err) => {
-        if (err) return console.log(err.message);
-        countNum++;
-        console.log(`${countNum} random dog image saved to file`);
-      });
-      //} else {
-      //   samePic++;
-      //   console.log(samePic);
-      //   counter--;
-      // }
-    })
-    .catch((err) => {
-      countErr++;
-      console.log(`There is ${countErr} error!!!!`);
-      if (!ifPrintErr) {
-        console.log(err);
-        ifPrintErr = true;
-      }
+const appendFilePro = (file, string) => {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(file, `${string}\n`, (err) => {
+      if (err) reject("An error appending file");
+      resolve();
     });
-}
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`breed name is ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return appendFilePro(`${__dirname}/dog-image.txt`, res.body.message);
+  })
+  .then(() => {
+    countNum++;
+    console.log(`${countNum} random dog image saved to file`);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// while (counter < 500) {
+//   counter++;
+//   superagent
+//     .get(`https://dog.ceo/api/breeds/image/random`)
+//     .then((res) => {
+//       fs.appendFile("dog-image.txt", `${res.body.message}\n`, (err) => {
+//         if (err) return console.log(err.message);
+//         countNum++;
+//         console.log(`${countNum} random dog image saved to file`);
+//       });
+//     })
+//     .catch((err) => {
+//       countErr++;
+//       console.log(`There is ${countErr} error!!!!`);
+//       if (!ifPrintErr) {
+//         console.log(err);
+//         ifPrintErr = true;
+//       }
+//     });
+// }
